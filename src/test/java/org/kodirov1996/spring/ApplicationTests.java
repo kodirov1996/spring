812c3b878384925188f27d7a4f80dc68dc5fca21
Qodirov <qodirovbek1996@gmail.com>
2021-09-09 14:48:56 +0500

    commit

---
+++ src/test/java/org/kodirov1996/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kodirov1996.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
